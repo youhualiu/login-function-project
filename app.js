f164b57557a9c3ba26aab8a4b81098023e8be56f
youhua <106305013@nccu.edu.tw>
2021-04-29 13:14:46 +0800

    feat: add authentication function

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const User = require("./models/user")
 
 // views engine setting
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // connecting to db
 mongoose.connect('mongodb://localhost/login-user', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,7 +31,30 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  res.render('index')
+  const inputEmail = req.body.email
+  const inputPassword = req.body.password
+  if (!inputEmail || !inputPassword) {
+    let incompletedWarning = 'The form is not completed'
+    console.log(inputEmail)
+    res.render('index', { incompletedWarning })
+    return
+  }
+  return User.findOne({ email: inputEmail })
+    .lean()
+    .then(user => {
+      if (user === null) {
+        let mistakeWarning = "User account doesn't exist"
+        res.render('index', { mistakeWarning })
+        return
+      } else if (user.password !== inputPassword) {
+        let mistakeWarning = "Password is wrong"
+        res.render('index', { mistakeWarning })
+        return
+      }
+      let name = user.firstName || null
+      res.render('welcome', { name })
+    })
+    .catch(error => console.log(error))
 })
 
 
